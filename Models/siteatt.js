14f04d45a532cde5ed9973afedac72397996f8f8
unknown <itsoxama@gmail.com>
2023-03-25 23:48:52 +0500

    first commit

---
+++ Models/siteatt.js
@@ -0,0 +1,71 @@
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Siteatten = new Schema({
+   
+    username: {
+        type: String,
+        
+    },
+    
+    userid: {
+        type: String
+    }
+    ,
+    projectid:{
+        type:String,
+    },
+    chkouttime:{
+        type:String
+    }
+    ,
+    workinghours:{
+        type:String
+    }
+    ,
+    
+    wages:{
+        
+        type:String,
+    },
+    date:{
+        
+        type:String,
+    },
+    time:{
+        
+        type:String,
+    },
+    
+    projectname:{
+        
+        type:String,
+    },
+    workingtime:{
+        type:Number
+    }
+    ,
+    
+    late:{
+        type:String
+    }
+    ,
+    
+    status: {
+        type: String
+    },
+
+},{
+    timestamps:true
+});
+
+
+
+
+
+
+const Siteatt = mongoose.model('Siteatten', Siteatten);
+module.exports =Siteatt
+  
